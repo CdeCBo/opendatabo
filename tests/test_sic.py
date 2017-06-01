462449e74aac0d2ae649a0772b263f8174384a51
Gabriel Garcia <garciat@live.com>
2017-06-01 13:18:38 -0400

    Test: use a smaller dataset

--- tests/test_sic.py
+++ tests/test_sic.py
@@ -3,7 +3,7 @@ import os
 from opendatabo.sic import save_market_prices
 
 
-def test_save_scz_2014():
-    save_market_prices('2014', 'sc', '/tmp/hello.csv', 'csv')
+def test_save_scz_2008():
+    save_market_prices('2008', 'sc', '/tmp/hello.csv', 'csv')
 
     assert os.path.exists('/tmp/hello.csv')
